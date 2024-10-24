a74e518fa7ec60f9c8f607235e761afda294c066
Chebel405 <anne.chebel@gmail.com>
2024-10-24 09:27:51 +0200

    Creation de la table relationnelle Location

--- src/main/java/com/example/LocationsBack/Entity/Client.java
+++ src/main/java/com/example/LocationsBack/Entity/Client.java
@@ -8,6 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.xml.stream.Location;
+import java.util.List;
+
 
 @Entity
 @Table(name = "client")
@@ -22,19 +25,28 @@ public class Client {
     private Long id;
 
     @JsonProperty("name")
+    @Column(name = "name", nullable = false)
     private String name;
 
     @JsonProperty("lastName")
+    @Column(name = "lastname", nullable = false)
     private String lastName;
 
     @JsonProperty("birthday")
+    @Column(name = "birthday", nullable = false)
     private String birthday;
 
     @JsonProperty("email")
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
     @JsonProperty("phone")
+    @Column(name = "phone", nullable = false)
     private String phone;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    //Liste des locations associées au client
+    private List<Location> locations;
 
 }
 
---
+++ src/main/java/com/example/LocationsBack/Entity/Location.java
@@ -0,0 +1,44 @@
+package com.example.LocationsBack.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "location")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToOne
+    @JoinColumn(name = "vehicule_id", nullable = false)
+    private Vehicule vehicule;
+
+    @Column(name = "date_debut", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dateDebut;
+
+    @Column(name = "date_fin", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date dateFin;
+
+    @Column(name = "prix_total", nullable = false)
+    private double prixTotal;
+
+
+
+
+}
